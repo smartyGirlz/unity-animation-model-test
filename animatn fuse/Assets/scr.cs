ba92d26b86b6bf4d1e6d4b2b0f56b88999543d80
smartyGirlz <35099078+smartyGirlz@users.noreply.github.com>
2019-02-22 17:28:33 +0530

    //

---
+++ animatn fuse/Assets/scr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+
+
+    public Animator a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        if(Input.GetMouseButton(0))
+        a.SetInteger("int",1);
+    }
+}
